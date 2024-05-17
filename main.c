960efd15920704439990eb860a4c9751fab19496
DeniedSaBUro <168509447+DeniedSaBUro@users.noreply.github.com>
2024-05-17 15:22:31 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -4,26 +4,51 @@
 #include <sys/wait.h>
 #include "fun.h"
 
-
-size_t n = 11;
+int p[2];
+int o[2];
 
 void run_fib(){
-    size_t res = fibonacci(n);
-    printf("(SUBPROCESS) Fibonacci of %ld is %ld.\n", n, res);
+    size_t m = 0;
+
+    close(p[1]);
+    size_t sz = read(p[0], &m, sizeof(m));
+    close(p[0]);
+    if (sz!=sizeof(m)){
+        printf("Cannot read all the data\n");
+        _exit(1);
+    }
+    size_t res = fibonacci(m);
+    printf("(SUBPROCESS) Fibonacci number %ld is %ld\n", m, res);
+    close(o[0]);
+    write(o[1], &res, sizeof(res));
+    close(o[1]);
 }
+
+size_t n = 12;
 
 int main(){
     printf("Hello, World!\n");
     size_t cpid;
 
+    pipe(p);
+    pipe(o);
+
     if ((cpid = fork()) == 0){
         run_fib();
     }
     else {
-        size_t res = fibonacci(n);
-        printf("Fibonacci of %ld is %ld.\n", n, res);
+        close(p[0]);
+        write(p[1], &n, sizeof(n));
+        close(p[1]);
+
+        size_t rc;
+
+        close(o[1]);
+        read(o[0], &rc, sizeof(rc));
+        close(o[0]);
+        printf("Fibonacci number %ld is %ld. (Taken from child process)\n", n, rc);
     }
-    if (cpid != 0){
+    if (cpid!=0){
         int rc;
         waitpid(cpid, &rc, 0);
         printf("A Child process killed.\n");
